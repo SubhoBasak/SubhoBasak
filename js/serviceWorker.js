1e81e4a556e17825911e61ad9ad5709d82f93fd7
SubhoBasak <subhobasak50@gmail.com>
2022-01-24 16:55:34 +0530

    added favicon and pwa

---
+++ js/serviceWorker.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = "gs-website-pwa";
+var urlsToCache = [
+  "/index.html",
+  "/about.html",
+  "/education.html",
+  "/skills.html",
+  "/gallery.html",
+  "/gallery2.html",
+  "/contact.html",
+  "/sitemap.html",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  var cacheWhitelist = ["gs-website-pwa"];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
